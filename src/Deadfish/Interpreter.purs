937cdf2c6d52356909775228488218f1e351bcef
Dennis Heckman <denheck@gmail.com>
2017-11-05 09:01:10 -0600

    finish deadfish interpreter

--- src/Deadfish/Interpreter.purs
+++ src/Deadfish/Interpreter.purs
@@ -3,7 +3,7 @@ module Deadfish.Interpreter (run) where
 import Data.Foldable (foldl)
 import Data.String (toCharArray)
 import Data.Tuple (Tuple(..))
-import Prelude (($), (+), (-))
+import Prelude (show, ($), (*), (+), (-), (<>))
 
 type Input = String
 type Output = String
@@ -14,6 +14,13 @@ run input register =
   foldl interpret (Tuple "" register) (toCharArray input)
 
 interpret :: Tuple Output Register -> Char -> Tuple Output Register
-interpret (Tuple output register) 'i' = Tuple output $ register + 1
-interpret (Tuple output register) 'd' = Tuple output $ register - 1
-interpret result _ = result
+interpret result char = resetRegister $ interpret' result char
+  where
+    resetRegister (Tuple output 256) = Tuple output 0
+    resetRegister (Tuple output -1) = Tuple output 0
+    resetRegister result = result
+    interpret' (Tuple output register) 'i' = Tuple output $ register + 1
+    interpret' (Tuple output register) 'd' = Tuple output $ register - 1
+    interpret' (Tuple output register) 's' = Tuple output $ register * register
+    interpret' (Tuple output register) 'o' = Tuple (output <> (show register)) register
+    interpret' result _ = result
--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,6 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Tuple (Tuple(..))
 import Deadfish.Interpreter as Deadfish
 import Test.Spec (describe, it)
@@ -19,3 +18,11 @@ main = do
         shouldEqual (Deadfish.run "i" 0) $ Tuple "" 1
       it "decrements the register by 1" do
         shouldEqual (Deadfish.run "d" 1) $ Tuple "" 0
+      it "squares the register" do
+        shouldEqual (Deadfish.run "s" 2) $ Tuple "" 4
+      it "outputs the register" do
+        shouldEqual (Deadfish.run "o" 5) $ Tuple "5" 5
+      it "register set to 0 when equal to 256" do
+        shouldEqual (Deadfish.run "i" 255) $ Tuple "" 0
+      it "register set to 0 when equal to -1" do
+         shouldEqual (Deadfish.run "d" 0) $ Tuple "" 0
